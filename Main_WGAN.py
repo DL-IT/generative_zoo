c4fac9fd955bf4a247a8882651ec8f72afc874cf
Vishwak Srinivasan <cs15btech11043@iith.ac.in>
2017-07-05 11:40:08 +0900

    WGAN minor changes.

--- Main_WGAN.py
+++ Main_WGAN.py
@@ -24,7 +24,7 @@ parser.add_argument('--max_gen_iter', type=int, default=200000, help='input the 
 parser.add_argument('--gen_lr', type=float, default=5e-05, help='input the learning rate for the generator')
 parser.add_argument('--dis_lr', type=float, default=5e-05, help='input the learning rate for the discriminator')
 parser.add_argument('--ngpu', type=int, default=0, help='input the number of GPUs required')
-parser.add_argument('--dcgan', required=True, type=bool, help='input the requirement of DCGAN')
+parser.add_argument('--dcgan', required=True, action='store_true', help='input the requirement of DCGAN')
 parser.add_argument('--dis_iter', type=int, default=5, help='input the number of discriminator iterations per generator iteration')
 opt	= parser.parse_args()
 print(opt)
@@ -70,7 +70,7 @@ else:
 # Place holders to ensure correct dimensions
 
 inpt	= t.FloatTensor(b_size, n_chan, img_size, img_size)
-if opt.dcgan == True:
+if opt.dcgan:
 	noise	= t.FloatTensor(b_size, n_z, 1, 1)
 	fixed_noise	= t.randn(b_size, n_z, 1, 1)
 else:
@@ -90,58 +90,86 @@ if ngpu > 0:
 	G_net	= G_net.cuda()
 	D_net	= D_net.cuda()
 	fixed_noise	= fixed_noise.cuda()
+	
+def de_sigmoid(tensor):
+	pass_	= tensor
+	pass_	= tensor.div(1 - tensor)
+	assert pass_.size() == tensor.size()
+	pass_	= t.log(pass_)
+	return pass_
+		
+gen_iters	= 0
 
-gen_iterations	= 0
-
-while True:
-	d_iter	= iter(d_loader)
+while gen_iters < opt.max_gen_iter:
+	
+	d_iter	 = iter(d_loader)
 	i	= 0
 	while i < len(d_loader):
-
-		# Train discriminator
-		j	= 0
-		while j < opt.dis_iter and i < len(d_loader):
-			j	= j + 1
-			data	= d_iter.next()
-			i	= i + 1
+		
+		# To avoid problems while training the Generator, the requires_grad is flipped for the parameters of the discriminator
+		for params in D_net.parameters():
+			params.requires_grad = True
 			
-			D_net.zero_grad()
-
-			# Real data
-			tr_x, _	= data
-			inpt.copy_(tr_x)
-			inptv	= V(inpt)
-			err_D_r	= D_net(inptv)
+		dis_iter	= opt.dis_iter
 			
-			# Fake data
-			noise.copy_(t.randn(noise.size()))
-			noisev	= V(noise)
-			fake	= G_net(noisev)
-			err_D_f	= D_net(fake)
-			
-			err_D	= err_D_r - err_D_f
-			err_D	= (err_D.mean(0)).view(1)
-			err_D.backward()
-			D_optim.step()
+		j =	0
+		# Train the discriminator 
+		while j < dis_iter and i < len(d_loader):
 			
 			for params in D_net.parameters():
 				params.data.clamp_(-0.01, 0.01)
+
+			D_net.zero_grad()
+			curdata	= d_iter.next()
+			i	= i + 1
 			
-		# Train Generator
-		gen_iterations	= gen_iterations + 1
+			# Train Discriminator with real	
+			X, _	= curdata
+			inpt.copy_(X)
+			inptv	= V(inpt)
+			
+			err_D_r	= D_net(inptv)
+			err_D_r = de_sigmoid(err_D_r)
+			err_D_r	= (err_D_r.mean(0)).view(1)
+
+			# Train Discriminator with fake
+			noise.copy_(t.randn(noise.size()))
+			noisev	= V(noise)
+
+			for params in G_net.parameters():
+				params.requires_grad = False
+
+			fake	= G_net(noisev)
+			err_D_f	= D_net(fake)
+			err_D_f	= de_sigmoid(err_D_f)
+			err_D_f	= (err_D_f.mean(0)).view(1)
+			
+			err_D	= err_D_r - err_D_f
+			err_D.backward()
+			D_optim.step()
+			j	= j + 1
+
+			for params in G_net.parameters():
+				params.requires_grad = True
+			
+		# Train the generator
+		for params in D_net.parameters():
+			params.requires_grad = False
+			
+		G_net.zero_grad()
 		noise.copy_(t.randn(noise.size()))
 		noisev	= V(noise)
 		fake	= G_net(noisev)
 		err_G	= D_net(fake)
+		err_G	= de_sigmoid(err_G)
 		err_G	= (err_G.mean(0)).view(1)
 		err_G.backward()
 		G_optim.step()
 		
-		if gen_iterations == opt.max_gen_iter:
-			break
+		gen_iters	= gen_iters + 1
 		
-		print('[%d/%d] Loss_D: %.4f Loss_G: %.4f' % (gen_iterations, opt.max_gen_iter, err_D.data[0], err_G.data[0]))
-			
-		if gen_iterations % 500 == 0:
-			fake	= G_net(V(fixed_noise))
-			tv_utils.save_image(fake.data, 'fake_samples-gen_iter={0}.png'.format(gen_iterations))
+		print('[{0}/{1}]\tLoss_D: {2}\tLoss_G: {3}'.format(gen_iters, opt.max_gen_iter, err_D.data[0], err_G.data[0]))
+		if gen_iters % 500 == 0:
+			fake		= G_net(V(fixed_noise, volatile=True))
+			fake.data	= fake.data.mul(0.5).add(0.5)
+			tv_utils.save_image(fake.data, 'fake_samples_gen_iter={0}.png'.format(gen_iters))
