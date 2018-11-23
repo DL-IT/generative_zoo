3c05826a2191086416cd73a0520c8096aea8263a
vishwakftw <cs15btech11043@iith.ac.in>
2018-11-23 18:20:42 +0530

    Fix architectures code - tabs to spaces - max line length = 120 - remove spaces at the end of a line

--- Architectures/CIFAR10.py
+++ Architectures/CIFAR10.py
@@ -1,158 +1,158 @@
-import torch as t
 import torch.nn as nn
 
+
 class Generator(nn.Module):
-	"""
-		Selected Generator for CIFAR10 dataset. This produces images of size 32 x 32.
-		Use this module for any GANs to be trained on CIFAR10.
-	"""
-	def __init__(self, n_z, ngpu, gen_type):
-		"""
-		Function to construct a Generator instance
-		Args:
-			n_z		: Dimensionality of the noise
-			ngpu		: Number of GPUs to be used
-			gen_type	: Type of Generator: 
-						'conv' for normal convolutional,
-						'resi' for residual blocks
-		"""
-		super(Generator, self).__init__()
+    """
+        Selected Generator for CIFAR10 dataset. This produces images of size 32 x 32.
+        Use this module for any GANs to be trained on CIFAR10.
+    """
+    def __init__(self, n_z, ngpu, gen_type):
+        """
+        Function to construct a Generator instance
+        Args:
+            n_z  : Dimensionality of the noise
+            ngpu : Number of GPUs to be used
+            gen_type : Type of Generator:
+                       'conv' for normal convolutional,
+                       'resi' for residual blocks
+        """
+        super(Generator, self).__init__()
 
-		assert ngpu >= 0, "Number of GPUs has to be non-negative"
-		assert n_z > 0, "Dimensionality of the noise vector has to be positive"
-		
-		if gen_type == 'conv':
-			# Architecture: Specified as follows:
-			# |   INPUT 	---->	  OUTPUT	(	   ACTIVATIONS 		  ) |
-			# |    n_z	---->	   4096		(BATCHNORM_1D, LEAKY_RELU, DROPOUT) |
-			# |  4x4x256	---->    8X8X128	(       BATCHNORM_2D, RELU 	  ) |
-			# |  8x8x128	---->    16x16x64	(       BATCHNORM_2D, RELU	  ) |
-			# |  16X16X64	---->	 32X32X3	(       BATCHNORM_2D, TANH	  ) |
-			dropout_prob	= 0.4
-			leaky_coeff	= 0.2
-			
-			# Fully Connected Section
-			layer	= 1
-			main	= nn.Sequential()
-			main.add_module('Linear_{0}-{1}-{2}'.format(layer, n_z, 4096), nn.Linear(n_z, 4096))
-			main.add_module('BatchNorm1d_{0}-{1}'.format(layer, 4096), nn.BatchNorm1d(4096))
-			main.add_module('LeakyReLU_{0}'.format(layer), nn.LeakyReLU(leaky_coeff, inplace=True))
-			main.add_module('Dropout_{0}'.format(layer), nn.Dropout(dropout_prob))
-			self.fc	= main
-			
-			# Convolutional Section
-			layer	= layer + 1
-			main	= nn.Sequential()
-			main.add_module('ConvTranspose2d_{0}-{1}-{2}'.format(layer, 256, 128),
-					nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1))
-			main.add_module('BatchNorm2d_{0}-{1}'.format(layer, 128), nn.BatchNorm2d(128))
-			main.add_module('ReLU_{0}'.format(layer), nn.ReLU(True))
-		
-			layer	= layer + 1
-			main.add_module('ConvTranspose2d_{0}-{1}-{2}'.format(layer, 128, 64),
-					nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4, stride=2, padding=1))
-			main.add_module('BatchNorm2d_{0}-{1}'.format(layer, 64), nn.BatchNorm2d(64))
-			main.add_module('ReLU_{0}'.format(layer), nn.ReLU(True))
-			
-			layer	= layer + 1
-			main.add_module('ConvTranspose2d_{0}-{1}-{2}'.format(layer, 64, 3),
-					nn.ConvTranspose2d(in_channels=64, out_channels=3, kernel_size=4, stride=2, padding=1))
-			main.add_module('BatchNorm2d_{0}-{1}'.format(layer, 64), nn.BatchNorm2d(3))
-			main.add_module('TanH_{0}'.format(layer), nn.Tanh())
-			self.cv	= main
-		
-		elif gen_type == 'resi':
-			raise NotImplementedError
-			
-		self.n_z	= n_z
-		self.ngpu	= ngpu
-		
-	def forward(self, input):
-		input	= input.view(-1, self.n_z)
-		if self.ngpu > 1:
-			pass_	= nn.parallel.data_parallel(self.fc, input, range(0, self.ngpu))
-			pass_	= pass_.view(-1, 256, 4, 4)
-			pass_	= nn.parallel.data_parallel(self.cv, pass_, range(0, self.ngpu))
-			return pass_
-		else:
-			pass_	= self.fc(input)
-			pass_	= pass_.view(-1, 256, 4, 4)
-			pass_	= self.cv(pass_)
-			return pass_
-			
+        assert ngpu >= 0, "Number of GPUs has to be non-negative"
+        assert n_z > 0, "Dimensionality of the noise vector has to be positive"
+
+        if gen_type == 'conv':
+            # Architecture: Specified as follows:
+            # |   INPUT     ---->      OUTPUT    (           ACTIVATIONS            ) |
+            # |    n_z      ---->       4096     ( BATCHNORM_1D, LEAKY_RELU, DROPOUT) |
+            # |  4x4x256    ---->     8X8X128    (         BATCHNORM_2D, RELU       ) |
+            # |  8x8x128    ---->     16x16x64   (         BATCHNORM_2D, RELU       ) |
+            # |  16X16X64   ---->     32X32X3    (         BATCHNORM_2D, TANH       ) |
+            dropout_prob = 0.4
+            leaky_coeff = 0.2
+
+            # Fully Connected Section
+            layer = 1
+            main = nn.Sequential()
+            main.add_module('Linear_{0}-{1}-{2}'.format(layer, n_z, 4096), nn.Linear(n_z, 4096))
+            main.add_module('BatchNorm1d_{0}-{1}'.format(layer, 4096), nn.BatchNorm1d(4096))
+            main.add_module('LeakyReLU_{0}'.format(layer), nn.LeakyReLU(leaky_coeff, inplace=True))
+            main.add_module('Dropout_{0}'.format(layer), nn.Dropout(dropout_prob))
+            self.fc = main
+
+            # Convolutional Section
+            layer = layer + 1
+            main = nn.Sequential()
+            main.add_module('ConvTranspose2d_{0}-{1}-{2}'.format(layer, 256, 128),
+                            nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1))
+            main.add_module('BatchNorm2d_{0}-{1}'.format(layer, 128), nn.BatchNorm2d(128))
+            main.add_module('ReLU_{0}'.format(layer), nn.ReLU(True))
+
+            layer = layer + 1
+            main.add_module('ConvTranspose2d_{0}-{1}-{2}'.format(layer, 128, 64),
+                            nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4, stride=2, padding=1))
+            main.add_module('BatchNorm2d_{0}-{1}'.format(layer, 64), nn.BatchNorm2d(64))
+            main.add_module('ReLU_{0}'.format(layer), nn.ReLU(True))
+
+            layer = layer + 1
+            main.add_module('ConvTranspose2d_{0}-{1}-{2}'.format(layer, 64, 3),
+                            nn.ConvTranspose2d(in_channels=64, out_channels=3, kernel_size=4, stride=2, padding=1))
+            main.add_module('BatchNorm2d_{0}-{1}'.format(layer, 64), nn.BatchNorm2d(3))
+            main.add_module('TanH_{0}'.format(layer), nn.Tanh())
+            self.cv = main
+
+        elif gen_type == 'resi':
+            raise NotImplementedError
+
+        self.n_z = n_z
+        self.ngpu = ngpu
+
+    def forward(self, input):
+        input = input.view(-1, self.n_z)
+        if self.ngpu > 1:
+            pass_ = nn.parallel.data_parallel(self.fc, input, range(0, self.ngpu))
+            pass_ = pass_.view(-1, 256, 4, 4)
+            pass_ = nn.parallel.data_parallel(self.cv, pass_, range(0, self.ngpu))
+            return pass_
+        else:
+            pass_ = self.fc(input)
+            pass_ = pass_.view(-1, 256, 4, 4)
+            pass_ = self.cv(pass_)
+            return pass_
+
+
 class Discriminator(nn.Module):
-	"""
-		Selected Discriminator for CIFAR10 dataset. This discriminates images of size 32 x 32.
-		Use this module for any GANs to be trained on CIFAR10.
-	"""
-	def __init__(self, ngpu, dis_type):
-		"""
-		Function to construct a Discriminator instance
-		Args:
-			ngpu		: Number of GPUs to be used
-			dis_type	: Type of Discriminator:
-						'conv' for normal convolutional,
-						'resi' for residual blocks
-		"""
-		super(Discriminator, self).__init__()
+    """
+        Selected Discriminator for CIFAR10 dataset. This discriminates images of size 32 x 32.
+        Use this module for any GANs to be trained on CIFAR10.
+    """
+    def __init__(self, ngpu, dis_type):
+        """
+        Function to construct a Discriminator instance
+        Args:
+            ngpu : Number of GPUs to be used
+            dis_type : Type of Discriminator:
+                       'conv' for normal convolutional,
+                       'resi' for residual blocks
+        """
+        super(Discriminator, self).__init__()
 
-		assert ngpu >= 0, "Number of GPUs has to be non-negative"
+        assert ngpu >= 0, "Number of GPUs has to be non-negative"
 
-		if dis_type == 'conv':
-			# Architecture: Specified as follows: 
-			# |   INPUT 	---->	  OUTPUT	(	   ACTIVATIONS 		  ) |
-			# |  32x32x3	---->	 16x16x64	(BATCHNORM_2D, LEAKY_RELU, DROPOUT) |
-			# |  16X16X64	---->    8X8X128	(BATCHNORM_2D, LEAKY_RELU, DROPOUT) |
-			# |  8x8x128	---->    4x4x256	(      BATCHNORM_2D, LEAKY_RELU	  ) |
-			# |   4096	---->	    1		(     	    SIGMOID		  ) |
-			dropout_prob	= 0.4
-			leaky_coeff	= 0.2
+        if dis_type == 'conv':
+            # Architecture: Specified as follows:
+            # |   INPUT     ---->      OUTPUT   (            ACTIVATIONS            ) |
+            # |  32x32x3    ---->     16x16x64  ( BATCHNORM_2D, LEAKY_RELU, DROPOUT ) |
+            # |  16X16X64   ---->     8X8X128   ( BATCHNORM_2D, LEAKY_RELU, DROPOUT ) |
+            # |  8x8x128    ---->     4x4x256   (      BATCHNORM_2D, LEAKY_RELU     ) |
+            # |   4096      ---->      1        (              SIGMOID              ) |
+            dropout_prob = 0.4
+            leaky_coeff = 0.2
 
-			# Convolution Section
-			layer	= 1
-			main	= nn.Sequential()
-			main.add_module('Conv2d_{0}-{1}-{2}'.format(layer, 3, 64), 
-			nn.Conv2d(in_channels=3, out_channels=64, kernel_size=4, stride=2, padding=1))
-			main.add_module('BatchNorm2d_{0}-{1}'.format(layer, 64), nn.BatchNorm2d(64))
-			main.add_module('LeakyReLU_{0}'.format(layer), nn.LeakyReLU(leaky_coeff, inplace=True))
-			main.add_module('Dropout_{0}'.format(layer), nn.Dropout(dropout_prob))
+            # Convolution Section
+            layer = 1
+            main = nn.Sequential()
+            main.add_module('Conv2d_{0}-{1}-{2}'.format(layer, 3, 64),
+                            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=4, stride=2, padding=1))
+            main.add_module('BatchNorm2d_{0}-{1}'.format(layer, 64), nn.BatchNorm2d(64))
+            main.add_module('LeakyReLU_{0}'.format(layer), nn.LeakyReLU(leaky_coeff, inplace=True))
+            main.add_module('Dropout_{0}'.format(layer), nn.Dropout(dropout_prob))
 
-			layer	= layer + 1
-			main.add_module('Conv2d_{0}-{1}-{2}'.format(layer, 64, 128),
-					nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1))
-			main.add_module('BatchNorm2d_{0}-{1}'.format(layer, 128), nn.BatchNorm2d(128))
-			main.add_module('LeakyReLU_{0}'.format(layer), nn.LeakyReLU(leaky_coeff, inplace=True))
-			main.add_module('Dropout_{0}'.format(layer), nn.Dropout(dropout_prob))
-		
-			layer	= layer + 1
-			main.add_module('Conv2d_{0}-{1}-{2}'.format(layer, 128, 256),
-					nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1))
-			main.add_module('BatchNorm2d_{0}-{1}'.format(layer, 256), nn.BatchNorm2d(256))
-			main.add_module('LeakyReLU_{0}'.format(layer), nn.LeakyReLU(leaky_coeff, inplace=True))
-			self.cv	= main
-		
-			# Fully connected Section
-			layer	= layer + 1
-			main	= nn.Sequential()
-			main.add_module('Linear_{0}-{1}-{2}'.format(layer, 4096, 1), nn.Linear(4096, 1))
-			main.add_module('Sigmoid_{0}'.format(layer), nn.Sigmoid())
-			self.fc	= main
-		
-			self.ngpu	= ngpu
+            layer = layer + 1
+            main.add_module('Conv2d_{0}-{1}-{2}'.format(layer, 64, 128),
+                            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1))
+            main.add_module('BatchNorm2d_{0}-{1}'.format(layer, 128), nn.BatchNorm2d(128))
+            main.add_module('LeakyReLU_{0}'.format(layer), nn.LeakyReLU(leaky_coeff, inplace=True))
+            main.add_module('Dropout_{0}'.format(layer), nn.Dropout(dropout_prob))
 
-		elif dis_type == 'resi':
-			raise NotImplementedError
-			
-		
-	def forward(self, input):
-		input	= input.view(-1, 3, 32, 32)
-		if self.ngpu > 1:
-			pass_	= nn.parallel.data_parallel(self.cv, input, range(0, self.ngpu))
-			pass_	= pass_.view(-1, 4096)
-			pass_	= nn.parallel.data_parallel(self.fc, pass_, range(0, self.ngpu))
-			return pass_
-		else:
-			pass_	= self.cv(input)
-			pass_	= pass_.view(-1, 4096)
-			pass_	= self.fc(pass_)
-			return pass_
+            layer = layer + 1
+            main.add_module('Conv2d_{0}-{1}-{2}'.format(layer, 128, 256),
+                            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1))
+            main.add_module('BatchNorm2d_{0}-{1}'.format(layer, 256), nn.BatchNorm2d(256))
+            main.add_module('LeakyReLU_{0}'.format(layer), nn.LeakyReLU(leaky_coeff, inplace=True))
+            self.cv = main
+
+            # Fully connected Section
+            layer = layer + 1
+            main = nn.Sequential()
+            main.add_module('Linear_{0}-{1}-{2}'.format(layer, 4096, 1), nn.Linear(4096, 1))
+            main.add_module('Sigmoid_{0}'.format(layer), nn.Sigmoid())
+            self.fc = main
+
+            self.ngpu = ngpu
+
+        elif dis_type == 'resi':
+            raise NotImplementedError
+
+    def forward(self, input):
+        input = input.view(-1, 3, 32, 32)
+        if self.ngpu > 1:
+            pass_ = nn.parallel.data_parallel(self.cv, input, range(0, self.ngpu))
+            pass_ = pass_.view(-1, 4096)
+            pass_ = nn.parallel.data_parallel(self.fc, pass_, range(0, self.ngpu))
+            return pass_
+        else:
+            pass_ = self.cv(input)
+            pass_ = pass_.view(-1, 4096)
+            pass_ = self.fc(pass_)
+            return pass_
--- Architectures/Generic.py
+++ Architectures/Generic.py
@@ -1,138 +1,146 @@
-import torch as t
 import torch.nn as nn
-# Generator net
+
 
 class Generator(nn.Module):
-	"""
-		Provides an architecture for Generator for any image based dataset. This produces images of size image_size x image_size
-		Use this module if you are using a very new dataset.
-		Motivated by DCGAN by Alec Radford et al.
-	"""
-	def __init__(self, image_size, n_z, n_chan, n_hidden, ngpu):
-		"""
-		Function to construct a Generator instance 
-		Args:
-			image_size		: Height / width of the real images
-			n_z			: Dimensionality of the latent space
-			n_chan			: Number of channels of the real images
-			n_hidden		: Number of feature maps in the first layer of the generator
-			ngpu			: Number of GPUs to be used
-		"""
-		super(Generator, self).__init__()
-		
-		assert image_size % 16 == 0, "Image size should be a multiple of 16"
-		assert ngpu >= 0, "Number of GPUs has to be non-negative"
-		assert n_z > 0, "Dimensionality of the latent space has to be positive"
-		
-		layer	= 1
-		main	= nn.Sequential()
-		
-		# Details to be followed:
-		# 1. ReLU for activation for all but the last layer
-		# 2. Batchnorm for all but the last layer
-		# 3. No fully connected layers
-		
-		# The first conv layer transforms the noise into a set of n_hidden feature maps
+    """
+        Provides an architecture for Generator for any image based dataset.
+        This produces images of size image_size x image_size
+        Use this module if you are using a very new dataset.
+        Motivated by DCGAN by Alec Radford et al.
+    """
+    def __init__(self, image_size, n_z, n_chan, n_hidden, ngpu):
+        """
+        Function to construct a Generator instance
+        Args:
+            image_size : Height / width of the real images
+            n_z : Dimensionality of the latent space
+            n_chan : Number of channels of the real images
+            n_hidden : Number of feature maps in the first layer of the generator
+            ngpu : Number of GPUs to be used
+        """
+        super(Generator, self).__init__()
 
-		main.add_module('conv_{0}-{1}-{2}'.format(layer, n_z, n_hidden), nn.ConvTranspose2d(n_z, n_hidden, kernel_size=4, stride=1, padding=0, bias=False))
-		main.add_module('batchnorm_{0}-{1}'.format(layer, n_hidden), nn.BatchNorm2d(n_hidden))
-		main.add_module('ReLU_{0}'.format(layer), nn.ReLU(True))
-		
-		# Current feature map size is 4
-		cur_size	= 4
-		
-		# Keep enlarging the feature map until before it reaches the size of the image
-		while cur_size < image_size//2 :
-			layer = layer + 1
-			main.add_module('conv_{0}-{1}-{2}'.format(layer, n_hidden, n_hidden//2), nn.ConvTranspose2d(n_hidden, n_hidden//2, kernel_size=4, stride=2, padding=1, bias=False))
-			main.add_module('batchnorm_{0}-{1}'.format(layer, n_hidden//2), nn.BatchNorm2d(n_hidden//2))
-			main.add_module('ReLU_{0}'.format(layer), nn.ReLU(True))
-			
-			n_hidden	= n_hidden // 2
-			cur_size	= cur_size * 2
-			
-		# The last conv layer transforms existing feature maps into n_chan feature maps of the size of the image
-		
-		layer	= layer + 1
-		main.add_module('conv_{0}-{1}-{2}'.format(layer, n_hidden, n_chan), nn.ConvTranspose2d(n_hidden, n_chan, kernel_size=4, stride=2, padding=1, bias=False))
-		main.add_module('TanH_{0}'.format(layer), nn.Tanh())
-		
-		self.main	= main
-		self.image_size	= image_size
-		self.n_z	= n_z
-		self.n_hidden	= n_hidden
-		self.n_chan	= n_chan
-		self.ngpu	= ngpu
-		
-	def forward(self, input):
-		if self.ngpu > 0:
-			output	= nn.parallel.data_parallel(self.main, input, range(0, self.ngpu))
-		else:
-			output	= self.main(input)			
-		return output
-		
+        assert image_size % 16 == 0, "Image size should be a multiple of 16"
+        assert ngpu >= 0, "Number of GPUs has to be non-negative"
+        assert n_z > 0, "Dimensionality of the latent space has to be positive"
+
+        layer = 1
+        main = nn.Sequential()
+
+        # Details to be followed:
+        # 1. ReLU for activation for all but the last layer
+        # 2. Batchnorm for all but the last layer
+        # 3. No fully connected layers
+
+        # The first conv layer transforms the noise into a set of n_hidden feature maps
+
+        main.add_module('conv_{0}-{1}-{2}'.format(layer, n_z, n_hidden),
+                        nn.ConvTranspose2d(n_z, n_hidden, kernel_size=4, stride=1, padding=0, bias=False))
+        main.add_module('batchnorm_{0}-{1}'.format(layer, n_hidden), nn.BatchNorm2d(n_hidden))
+        main.add_module('ReLU_{0}'.format(layer), nn.ReLU(True))
+
+        # Current feature map size is 4
+        cur_size = 4
+
+        # Keep enlarging the feature map until before it reaches the size of the image
+        while cur_size < image_size // 2:
+            layer = layer + 1
+            main.add_module('conv_{0}-{1}-{2}'.format(layer, n_hidden, n_hidden // 2),
+                            nn.ConvTranspose2d(n_hidden, n_hidden // 2, kernel_size=4, stride=2, padding=1, bias=False))
+            main.add_module('batchnorm_{0}-{1}'.format(layer, n_hidden // 2), nn.BatchNorm2d(n_hidden // 2))
+            main.add_module('ReLU_{0}'.format(layer), nn.ReLU(True))
+
+            n_hidden = n_hidden // 2
+            cur_size = cur_size * 2
+
+        # The last conv layer transforms existing feature maps into n_chan feature maps of the size of the image
+
+        layer = layer + 1
+        main.add_module('conv_{0}-{1}-{2}'.format(layer, n_hidden, n_chan),
+                        nn.ConvTranspose2d(n_hidden, n_chan, kernel_size=4, stride=2, padding=1, bias=False))
+        main.add_module('TanH_{0}'.format(layer), nn.Tanh())
+
+        self.main = main
+        self.image_size = image_size
+        self.n_z = n_z
+        self.n_hidden = n_hidden
+        self.n_chan = n_chan
+        self.ngpu = ngpu
+
+    def forward(self, input):
+        if self.ngpu > 0:
+            output = nn.parallel.data_parallel(self.main, input, range(0, self.ngpu))
+        else:
+            output = self.main(input)
+        return output
+
+
 class Discriminator(nn.Module):
-	"""
-		Provides an architecture for Discriminator for any image based dataset. This discriminates images of size image_size x image_size
-		Use this module if you are using a very new dataset.
-		Motivated by DCGAN by Alec Radford et al.
-	"""
-	def __init__(self, image_size, n_chan, n_hidden, ngpu):
-		"""
-		Function to construct a Generator instance 
-		Args:
-			image_size		: Height / width of the real images
-			n_chan			: Number of channels of the real images
-			n_hidden		: Number of feature maps in the first layer of the generator
-			ngpu			: Number of GPUs to be used
-		"""
-		super(Discriminator, self).__init__()
-	
-		assert image_size % 16 == 0, "Image size should be a multiple of 16"
-		assert ngpu >= 0, "Number of GPUs has to be non-negative"
-	
-		layer	= 1
-		main	= nn.Sequential()
-	
-		# Details to be followed:
-		# 1. Leaky ReLU activation for all but the first and last layer
-		# 2. Batchnorm for all but the last layer
-		# 3. No fully connected layers
-	
-		# The first conv layer transforms the image into a smaller feature map
-	
-		main.add_module('conv_{0}-{1}-{2}'.format(layer, n_chan, n_hidden), nn.Conv2d(n_chan, n_hidden, kernel_size=4, stride=2, padding=1, bias=False))
-		main.add_module('LeakyReLU_{0}'.format(layer), nn.LeakyReLU(0.2, inplace=True))
-		
-		# Current feature map size is image_size/2
-		
-		cur_size	= image_size // 2
-		
-		# Keep diminishing the size of feature map until before it reaches 4
-		while cur_size > 4:
-			layer	= layer + 1
-			main.add_module('conv_{0}-{1}-{2}'.format(layer, n_hidden, n_hidden*2), nn.Conv2d(n_hidden, n_hidden*2, kernel_size=4, stride=2, padding=1, bias=False))
-			main.add_module('batchnorm_{0}-{1}'.format(layer, n_hidden*2), nn.BatchNorm2d(n_hidden*2))
-			main.add_module('LeakyReLU_{0}'.format(layer), nn.LeakyReLU(0.2, inplace=True))
-			
-			cur_size	= cur_size // 2
-			n_hidden	= n_hidden * 2
-			
-		# The last conv layer transforms the K x 4 x 4 feature map into a K dimensional output vector
-		
-		layer	= layer + 1		
-		main.add_module('conv_{0}-{1}-{2}'.format(layer, n_hidden, 1), nn.Conv2d(n_hidden, 1, kernel_size=4, stride=1, padding=0, bias=False))
-		main.add_module('Sigmoid_{0}'.format(layer), nn.Sigmoid())
-		
-		self.main	= main
-		self.image_size	= image_size
-		self.n_hidden	= n_hidden
-		self.n_chan	= n_chan
-		self.ngpu	= ngpu
-		
-	def forward(self, input):
-		if self.ngpu > 1:
-			output	= nn.parallel.data_parallel(self.main, input, range(0, self.ngpu))
-		else:
-			output	= self.main(input)
-		return output.view(-1, 1)
+    """
+        Provides an architecture for Discriminator for any image based dataset.
+        This discriminates images of size image_size x image_size
+        Use this module if you are using a very new dataset.
+        Motivated by DCGAN by Alec Radford et al.
+    """
+    def __init__(self, image_size, n_chan, n_hidden, ngpu):
+        """
+        Function to construct a Generator instance
+        Args:
+            image_size : Height / width of the real images
+            n_chan : Number of channels of the real images
+            n_hidden : Number of feature maps in the first layer of the generator
+            ngpu : Number of GPUs to be used
+        """
+        super(Discriminator, self).__init__()
+
+        assert image_size % 16 == 0, "Image size should be a multiple of 16"
+        assert ngpu >= 0, "Number of GPUs has to be non-negative"
+
+        layer = 1
+        main = nn.Sequential()
+
+        # Details to be followed:
+        # 1. Leaky ReLU activation for all but the first and last layer
+        # 2. Batchnorm for all but the last layer
+        # 3. No fully connected layers
+
+        # The first conv layer transforms the image into a smaller feature map
+
+        main.add_module('conv_{0}-{1}-{2}'.format(layer, n_chan, n_hidden),
+                        nn.Conv2d(n_chan, n_hidden, kernel_size=4, stride=2, padding=1, bias=False))
+        main.add_module('LeakyReLU_{0}'.format(layer), nn.LeakyReLU(0.2, inplace=True))
+
+        # Current feature map size is image_size/2
+
+        cur_size = image_size // 2
+
+        # Keep diminishing the size of feature map until before it reaches 4
+        while cur_size > 4:
+            layer = layer + 1
+            main.add_module('conv_{0}-{1}-{2}'.format(layer, n_hidden, n_hidden * 2),
+                            nn.Conv2d(n_hidden, n_hidden * 2, kernel_size=4, stride=2, padding=1, bias=False))
+            main.add_module('batchnorm_{0}-{1}'.format(layer, n_hidden * 2), nn.BatchNorm2d(n_hidden * 2))
+            main.add_module('LeakyReLU_{0}'.format(layer), nn.LeakyReLU(0.2, inplace=True))
+
+            cur_size = cur_size // 2
+            n_hidden = n_hidden * 2
+
+        # The last conv layer transforms the K x 4 x 4 feature map into a K dimensional output vector
+
+        layer = layer + 1
+        main.add_module('conv_{0}-{1}-{2}'.format(layer, n_hidden, 1),
+                        nn.Conv2d(n_hidden, 1, kernel_size=4, stride=1, padding=0, bias=False))
+        main.add_module('Sigmoid_{0}'.format(layer), nn.Sigmoid())
+
+        self.main = main
+        self.image_size = image_size
+        self.n_hidden = n_hidden
+        self.n_chan = n_chan
+        self.ngpu = ngpu
+
+    def forward(self, input):
+        if self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(0, self.ngpu))
+        else:
+            output = self.main(input)
+        return output.view(-1, 1)
