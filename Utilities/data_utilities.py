d392dd5678f2f3c16dc346bf5a7bdf4ae0cb1279
vishwakftw <cs15btech11043@iith.ac.in>
2018-11-23 20:12:36 +0530

    Fix utilties code - tabs to spaces - max line length = 120 - remove spaces at the end of line

--- Utilities/data_utilities.py
+++ Utilities/data_utilities.py
@@ -2,76 +2,87 @@ from .cub200 import CUB2002010
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 
+
 def MNIST_loader(root, image_size, normalize=True):
-	"""
-		Function to load torchvision dataset object based on just image size
-		Args:
-			root		= If your dataset is downloaded and ready to use, mention the location of this folder. Else, the dataset will be downloaded to this location
-			image_size	= Size of every image
-			normalize	= Requirement to normalize the image. Default is true
-	"""
-	transformations	= [transforms.Scale(image_size), transforms.ToTensor()]
-	if normalize == True:
-		transformations.append(transforms.Normalize((0.5, ), (0.5, )))
-	mnist_data	= dset.MNIST(root=root, download=True, transform=transforms.Compose(transformations))
-	return mnist_data
-	
+    """
+        Function to load torchvision dataset object based on just image size
+        Args:
+            root = If your dataset is downloaded and ready to use, mention the location of this folder.
+                   Else, the dataset will be downloaded to this location
+            image_size = Size of every image
+            normalize = Requirement to normalize the image. Default is true
+    """
+    transformations = [transforms.Scale(image_size), transforms.ToTensor()]
+    if normalize:
+        transformations.append(transforms.Normalize((0.5, ), (0.5, )))
+    mnist_data = dset.MNIST(root=root, download=True, transform=transforms.Compose(transformations))
+    return mnist_data
+
+
 def CIFAR10_loader(root, image_size, normalize=True):
-	"""
-		Function to load torchvision dataset object based on just image size
-		Args:
-			root		= If your dataset is downloaded and ready to use, mention the location of this folder. Else, the dataset will be downloaded to this location
-			image_size	= Size of every image
-			normalize	= Requirement to normalize the image. Default is true
-	"""
-	transformations	= [transforms.Scale(image_size), transforms.ToTensor()]
-	if normalize == True:
-		transformations.append(transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)))
-	cifar10_data	= dset.CIFAR10(root=root, download=True, transform=transforms.Compose(transformations))
-	return cifar10_data
-	
+    """
+        Function to load torchvision dataset object based on just image size
+        Args:
+            root = If your dataset is downloaded and ready to use, mention the location of this folder.
+                   Else, the dataset will be downloaded to this location
+            image_size = Size of every image
+            normalize = Requirement to normalize the image. Default is true
+    """
+    transformations = [transforms.Scale(image_size), transforms.ToTensor()]
+    if normalize:
+        transformations.append(transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)))
+    cifar10_data = dset.CIFAR10(root=root, download=True, transform=transforms.Compose(transformations))
+    return cifar10_data
+
+
 def LSUN_loader(root, image_size, classes=['bedroom'], normalize=True):
-	"""
-		Function to load torchvision dataset object based on just image size
-		Args:
-			root		= If your dataset is downloaded and ready to use, mention the location of this folder. Else, the dataset will be downloaded to this location
-			image_size	= Size of every image
-			classes		= Default class is 'bedroom'. Other available classes are:
-						'bridge', 'church_outdoor', 'classroom', 'conference_room', 'dining_room', 'kitchen', 'living_room', 'restaurant', 'tower'
-			normalize	= Requirement to normalize the image. Default is true
-	"""
-	transformations	= [transforms.Scale(image_size), transforms.CenterCrop(image_size), transforms.ToTensor()]
-	if normalize == True:
-		transformations.append(transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)))
-	for c in classes:
-		c	= c + '_train'
-	lsun_data	= dset.LSUN(db_path=root, classes=classes, transform=transforms.Compose(transformations))
-	return lsun_data
-	
+    """
+        Function to load torchvision dataset object based on just image size
+        Args:
+            root = If your dataset is downloaded and ready to use, mention the location of this folder.
+                   Else, the dataset will be downloaded to this location
+            image_size = Size of every image
+            classes = Default class is 'bedroom'. Other available classes are:
+                      'bridge', 'church_outdoor', 'classroom', 'conference_room', 'dining_room',
+                      'kitchen', 'living_room', 'restaurant', 'tower'
+            normalize = Requirement to normalize the image. Default is true
+    """
+    transformations = [transforms.Scale(image_size), transforms.CenterCrop(image_size), transforms.ToTensor()]
+    if normalize:
+        transformations.append(transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)))
+    for c in classes:
+        c = c + '_train'
+    lsun_data = dset.LSUN(db_path=root, classes=classes, transform=transforms.Compose(transformations))
+    return lsun_data
+
+
 def CUB200_2010_loader(root, image_size, normalize=True):
-	"""
-		Function to load torchvision dataset object based on just image size
-		Args:
-			root		= If your dataset is downloaded and ready to use, mention the location of this folder. Else, the dataset will be downloaded to this location
-			image_size	= Size of every image
-			normalize	= Requirement to normalize the image. Default is true
-	"""
-	transformations	= [transforms.Scale(image_size), transforms.ToTensor()]
-	if normalize == True:
-		transformations.append(transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)))
-	cub200_2010_data	= CUB2002010(root=root, download=True, transform=transforms.Compose(transformations))
-	return cub200_2010_data
-	
+    """
+        Function to load torchvision dataset object based on just image size
+        Args:
+            root = If your dataset is downloaded and ready to use, mention the location of this folder.
+                   Else, the dataset will be downloaded to this location
+            image_size = Size of every image
+            normalize = Requirement to normalize the image. Default is true
+    """
+    transformations = [transforms.Scale(image_size), transforms.ToTensor()]
+    if normalize:
+        transformations.append(transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)))
+    cub200_2010_data = CUB2002010(root=root, download=True, transform=transforms.Compose(transformations))
+    return cub200_2010_data
+
+
 def FASHIONMNIST_loader(root, image_size, normalize=True):
-	"""
-		Function to load torchvision dataset object based on just image size
-		Args:
-			root		= If your dataset is downloaded and ready to use, mention the location of this folder. Else, the dataset will be downloaded to this location
-			image_size	= Size of every image
-			normalize	= Requirement to normalize the image. Default is true
-	"""
-	transformations	= [transforms.Scale(image_size), transforms.ToTensor()]
-	if normalize == True:
-		transformations.append(transforms.Normalize((0.5, ), (0.5, )))		
-	fash_mnist_data	= dset.FashionMNIST(root=root, download=True, transform=transforms.Compose(transformations))
-	return fash_mnist_data
+    """
+        Function to load torchvision dataset object based on just image size
+        Args:
+            root = If your dataset is downloaded and ready to use, mention the location of this folder.
+                   Else, the dataset will be downloaded to this location
+            image_size = Size of every image
+            normalize = Requirement to normalize the image. Default is true
+    """
+    transformations = [transforms.Scale(image_size), transforms.ToTensor()]
+    if normalize:
+        transformations.append(transforms.Normalize((0.5, ), (0.5, )))
+    fash_mnist_data = dset.FashionMNIST(root=root, download=True, transform=transforms.Compose(transformations))
+    return fash_mnist_data
--- Utilities/utilities.py
+++ Utilities/utilities.py
@@ -1,62 +1,68 @@
 import torch as t
 import numpy as np
 
-def get_optimizer_with_params(optimizer_details):
-	optim_name	= optimizer_details['name']
-	learn_rate	= optimizer_details['learn_rate']
-	optim_params	= optimizer_details['params']
-	
-	if 'name' not in optimizer_details.keys() or 'learn_rate' not in optimizer_details.keys() or 'params' not in optimizer_details.keys():
-		raise ValueError("Important optimizer details not mentioned")
 
-	if optim_name	== 'adadelta':
-		return t.optim.Adadelta(params=optim_params, lr=learn_rate)
-	if optim_name	== 'adagrad':
-		return t.optim.Adagrad(params=optim_params, lr=learn_rate)
-	if optim_name	== 'adam':
-		if 'betas' in optimizer_details.keys():
-			return t.optim.Adam(params=optim_params, lr=learn_rate, betas=optimizer_details['betas'])
-		else:
-			return t.optim.Adam(params=optim_params, lr=learn_rate)
-	if optim_name	== 'adamax':
-		if 'betas' in optimizer_details.keys():
-			return t.optim.Adamax(params=optim_params, lr=learn_rate, betas=optimizer_details['betas'])
-		else:
-			return t.optim.Adamax(params=optim_params, lr=learn_rate)
-	if optim_name	== 'rmsprop':
-		return t.optim.RMSprop(params=optim_params, lr=learn_rate)
-	if optim_name	== 'sgd':
-		if 'momentum' in optimizer_details.keys():
-			if 'nesterov' in optimizer_details.keys():
-				return t.optim.SGD(params=optim_params, lr=learn_rate, momentum=optimizer_details['momentum'], nesterov=optimizer_details['nesterov'])
-			else:
-				return t.optim.SGD(params=optim_params, lr=learn_rate, momentum=optimizer_details['momentum'])
-		else:
-			return t.optim.SGD(params=optim_params, lr=learn_rate)
-			
+def get_optimizer_with_params(optimizer_details):
+    optim_name = optimizer_details['name']
+    learn_rate = optimizer_details['learn_rate']
+    optim_params = optimizer_details['params']
+
+    if not ('name' in optimizer_details.keys() and 'learn_rate' in optimizer_details.keys()
+            and 'params' in optimizer_details.keys()):
+        raise ValueError("Important optimizer details not mentioned")
+
+    if optim_name == 'adadelta':
+        return t.optim.Adadelta(params=optim_params, lr=learn_rate)
+    if optim_name == 'adagrad':
+        return t.optim.Adagrad(params=optim_params, lr=learn_rate)
+    if optim_name == 'adam':
+        if 'betas' in optimizer_details.keys():
+            return t.optim.Adam(params=optim_params, lr=learn_rate, betas=optimizer_details['betas'])
+        else:
+            return t.optim.Adam(params=optim_params, lr=learn_rate)
+    if optim_name == 'adamax':
+        if 'betas' in optimizer_details.keys():
+            return t.optim.Adamax(params=optim_params, lr=learn_rate, betas=optimizer_details['betas'])
+        else:
+            return t.optim.Adamax(params=optim_params, lr=learn_rate)
+    if optim_name == 'rmsprop':
+        return t.optim.RMSprop(params=optim_params, lr=learn_rate)
+    if optim_name == 'sgd':
+        if 'momentum' in optimizer_details.keys():
+            if 'nesterov' in optimizer_details.keys():
+                return t.optim.SGD(params=optim_params, lr=learn_rate, momentum=optimizer_details['momentum'],
+                                   nesterov=optimizer_details['nesterov'])
+            else:
+                return t.optim.SGD(params=optim_params, lr=learn_rate, momentum=optimizer_details['momentum'])
+        else:
+            return t.optim.SGD(params=optim_params, lr=learn_rate)
+
+
 def weight_init_scheme(mdl):
-	mdl_name	= mdl.__class__.__name__
-	if mdl_name.find('Linear') != -1:
-		f_in	= mdl.weight.size(0)
-		f_out	= mdl.weight.size(1)
-		c	= np.sqrt(6/(f_in + f_out))
-		mdl.weight.data.uniform_(-c, c)
-		mdl.bias.data.fill_(0)
-	if mdl_name.find('Conv') != -1:
-		mdl.weight.data.normal_(0.0, 0.02)
-	if mdl_name.find('BatchNorm') != -1:
-		mdl.weight.data.normal_(1.0, 0.02)
-		mdl.bias.data.fill_(0)
-		
+    mdl_name = mdl.__class__.__name__
+    if mdl_name.find('Linear') != -1:
+        f_in = mdl.weight.size(0)
+        f_out = mdl.weight.size(1)
+        c = np.sqrt(6 / (f_in + f_out))
+        mdl.weight.data.uniform_(-c, c)
+        mdl.bias.data.fill_(0)
+    if mdl_name.find('Conv') != -1:
+        mdl.weight.data.normal_(0.0, 0.02)
+    if mdl_name.find('BatchNorm') != -1:
+        mdl.weight.data.normal_(1.0, 0.02)
+        mdl.bias.data.fill_(0)
+
+
 def KLD(means_and_logcovs):
-	means	= means_and_logcovs[0]
-	logcovs	= means_and_logcovs[1]
-	kld	= means.pow(2).add_(logcovs.exp()).mul(-1).add_(logcovs).add_(1)
-	kld	= t.sum(kld).mul_(-0.5)
-	return kld
-	
+    means = means_and_logcovs[0]
+    logcovs = means_and_logcovs[1]
+    kld = means.pow(2).add_(logcovs.exp()).mul(-1).add_(logcovs).add_(1)
+    kld = t.sum(kld).mul_(-0.5)
+    return kld
+
+
 def de_sigmoid(tensor):
-	pass_	= tensor
-	pass_	= pass_.div(1 - tensor)
-	pass_	= t.log(pass_)
-	return pass_
+    pass_ = tensor
+    pass_ = pass_.div(1 - tensor)
+    pass_ = t.log(pass_)
+    return pass_
